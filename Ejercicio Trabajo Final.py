254ccaf79d251df6ce7439c4ad13acadcd9fdca7
FabioLunaa <95083908+FabioLunaa@users.noreply.github.com>
2022-08-13 20:37:07 -0300

    Trabajo final Python Fabio Luna
    Trabajo final Python Fabio Luna

---
+++ Ejercicio Trabajo Final.py
@@ -0,0 +1,10 @@
+#Ejercicio 4: Crear una lista en Python denominado "Perro2" que contenga los 
+#             siguintes valores: 14, Fido, 12/12/2012, Macho, 23546987
+#             recorrer la lista Perro2 con un bucle y mostrar sus elementos por pantalla
+#             comenzando desde el último hasta el primero.
+
+Perro2 = [14, 'Fido', 12/12/2012, 'Macho', 23546987]
+
+for elementos in Perro2:
+    elementos.reverse()
+    print(elementos)
