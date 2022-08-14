664c33d210408aaaacf35ff35c1fc772548bc4d2
20fer20 <fjuare34@gmail.com>
2022-08-14 13:39:56 -0300

    Ej7

--- Ejercicio 7/historial3.py
+++ Ejercicio 7/historial3.py
@@ -1,13 +1,10 @@
-historial3 = (9530, 4120, 4580, 1500, 890,7516,426)
- suma = 0
- for valor in historial3: 
-    suma = suma + valor
-
-print(f"La suma es {suma}")
-
-cantidad_elementos = len(historial3)
-promedio = suma / cantidad_elementos
-
-if promedio > 4500 :
-    print(“Se ha excedido del gasto promedio para su mascota”)
-
+Historial3 = (9530, 4120, 4580, 1500, 890,7516,426)
+cant_serv = len(Historial3)
+monto_total= 0
+for monto in Historial3:
+    monto_total += monto
+    
+gasto_prom = monto_total / cant_serv
+print(gasto_prom)
+if gasto_prom > 4500:
+    print("Se ha excedido del gasto promedio para su mascota")
